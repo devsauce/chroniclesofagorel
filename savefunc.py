dcc7521bad641b7af074c43a76e0dea2b9b19715
devsauce <devofsauce@gmail.com>
2017-06-04 18:50:18 -0400

    Add files via upload

--- savefunc.py
+++ savefunc.py
@@ -1,3 +1,9 @@
 def load():
     f = open("save/pc.cf", 'r')
     return f
+def save(mainc):
+    f = open("save/pc.cf", 'w')
+    ts = "name: " + mainc.name + '\n' + "hp: " + str(mainc.mhp)+ '\n' + "strn: " + str(mainc.strn) + '\n' + "defn: " + str(mainc.defn) + '\n'
+    f.write(ts)
+    f.close()
+    return
