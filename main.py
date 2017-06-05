2c4d4f20f893f0103dadc164eea8269682687414
devsauce <devofsauce@gmail.com>
2017-06-05 16:49:34 -0400

    edited main.py
    includes startgame() function

--- main.py
+++ main.py
@@ -11,11 +11,30 @@ def l2b(lfile, mainc):
     mainc.strn = int(s[6:-1])
     s = lfile.readline()
     mainc.defn = int(s[6:-1])
+def startgame(mainc):
+    mainc.name = raw_input("What will your name be? ")
+    c = 0
+    while(c>0 and c<4):
+        c = raw_input("Warrior[1], Mage[2], or Rogue[3]? ")
+    if(c==1):
+        mainc.strn = 5
+        mainc.defn = 3
+        mainc.mhp = 20
+    elif(c==2):
+        mainc.strn = 3
+        mainc.defn = 3
+        mainc.mhp = 40
+    else:
+        mainc.strn = 3
+        mainc.defn = 5
+        mainc.mhp = 20
+    mainc.chp = mainc.mhp
+    return                                  ##PICK UP HERE##
 
 def norl(mainc):
     r = raw_input("[N]ew or [L]oad? ")
     if (r == 'n' or r == 'N'):
-        startgame()
+        startgame(mainc)
         return
     if (r == 'l' or r == 'L'):
         lfile = savefunc.load()
